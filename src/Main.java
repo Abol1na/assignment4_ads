1714f5f40ce054e0bcc2e738ba01c3c93d87cdf6
Abolina667 <abai.sagizhan.absi@gmail.com>
2023-05-14 22:06:56 +0600

    MyHashTable class

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
 
-        tree.insert(5);
-        tree.insert(3);
-        tree.insert(8);
-        tree.insert(1);
-        tree.insert(4);
-        tree.insert(7);
-        tree.insert(9);
-
-        tree.inOrder();
-
-        tree.remove(3);
-        tree.remove(5);
-        System.out.println();
-        tree.inOrder();
     }
 }
